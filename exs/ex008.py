a6aecd1b5f4fbb59b70e168c449a5f51b7ef0838
Vinicius Santos <2007viniciussantos@gmail.com>
2025-06-30 21:52:40 -0300

    medidor de distância em metros, tabuada, conversão de valores em reais para dólares feitos.,

---
+++ exs/ex008.py
@@ -0,0 +1,4 @@
+medida = float(input('Coloque uma distância em metros:'))
+cm = medida * 100
+mm = medida * 1000
+print('A distância de {} é de {} centímetros e {} melímetros.'.format(medida,cm,mm))
---
+++ exs/ex010.py
@@ -0,0 +1,3 @@
+num = float(input('Coloque um valor: R$'))
+cal = num / 5.43
+print('R${:.2f} é US{:.2f}'.format(num,cal))
