51e4e6a32abafc361f04e18cd3cccdbf368f3dd6
Vinicius Santos <2007viniciussantos@gmail.com>
2025-07-29 23:01:56 -0300

    colocando cores no terminal

---
+++ exs/ex035.py
@@ -0,0 +1,10 @@
+print('-------------')
+print('\033[4;34mAnalisador de Triângulos\033[m')
+print('-------------')
+r1 = float(input('Primeiro segmento:'))
+r2 = float(input('Segundo segmento:'))
+r3 = float(input('Terceiro segmento:'))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos acima podem formar um triângulo!')
+else:
+    print('Os segmentos acima não podem formar um triângulo!')
---
+++ tests/test011.py
@@ -0,0 +1,6 @@
+#TESTE DE CORES NO TERMINAL
+print('\033[31mHello, World!\033[m')
+print('\033[1;33;42[mSalve!\033[m')
+a = 4
+b = 45
+print('Eu tenho \033[35m{}\033[m bolas e \033[33m{}\033[m pipas'.format(a, b))
